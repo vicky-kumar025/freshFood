e53e28a29171327e00506ef4cf2517b39538ab3e
vicky-kumar025 <200983079+vicky-kumar025@users.noreply.github.com>
2026-09-18 23:46:08 +0530

    Update cookie settings for token security

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -27,8 +27,8 @@ export const signUp=async (req,res) => {
 
         const token=await genToken(user._id)
         res.cookie("token",token,{
-            secure:false,
-            sameSite:"strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7*24*60*60*1000,
             httpOnly:true
         })
@@ -55,8 +55,8 @@ export const signIn=async (req,res) => {
 
         const token=await genToken(user._id)
         res.cookie("token",token,{
-            secure:false,
-            sameSite:"strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7*24*60*60*1000,
             httpOnly:true
         })
@@ -142,8 +142,8 @@ export const googleAuth=async (req,res) => {
 
         const token=await genToken(user._id)
         res.cookie("token",token,{
-            secure:false,
-            sameSite:"strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7*24*60*60*1000,
             httpOnly:true
         })
